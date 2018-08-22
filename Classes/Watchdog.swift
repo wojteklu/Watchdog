09531357ebee1e282a28afb592311c6d211575a4
Ryan Baumbach <github@ryan.codes>
2018-08-22 00:03:11 -0600

    Add swift migration & the fixing of @objc inference (#44)
    - Migrate project using Xcode 9.3, update Swift version to 4.1, resolve deprecated @objc inference in Watchdog.swift
    - Update Swift version from 4.0 to 4.1 in README.md

--- Classes/Watchdog.swift
+++ Classes/Watchdog.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 /// Class for logging excessive blocking on the main thread.
-@objc final public class Watchdog: NSObject {
+final public class Watchdog: NSObject {
     fileprivate let pingThread: PingThread
 
-    public static let defaultThreshold = 0.4
+    @objc public static let defaultThreshold = 0.4
 
     /// Convenience initializer that allows you to construct a `WatchDog` object with default behavior.
     /// - parameter threshold: number of seconds that must pass to consider the main thread blocked.
     /// - parameter strictMode: boolean value that stops the execution whenever the threshold is reached.
-    public convenience init(threshold: Double = Watchdog.defaultThreshold, strictMode: Bool = false) {
+    @objc public convenience init(threshold: Double = Watchdog.defaultThreshold, strictMode: Bool = false) {
         let message = "👮 Main thread was blocked for " + String(format:"%.2f", threshold) + "s 👮"
 
         self.init(threshold: threshold) {
@@ -24,7 +24,7 @@ import Foundation
     /// Default initializer that allows you to construct a `WatchDog` object specifying a custom callback.
     /// - parameter threshold: number of seconds that must pass to consider the main thread blocked.
     /// - parameter watchdogFiredCallback: a callback that will be called when the the threshold is reached
-    public init(threshold: Double = Watchdog.defaultThreshold, watchdogFiredCallback: @escaping () -> Void) {
+    @objc public init(threshold: Double = Watchdog.defaultThreshold, watchdogFiredCallback: @escaping () -> Void) {
         self.pingThread = PingThread(threshold: threshold, handler: watchdogFiredCallback)
 
         self.pingThread.start()
